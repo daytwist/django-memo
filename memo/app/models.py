bf65f5d148429d167ae4ff86ce85bd1965070fb6
Natsumi Oishi <98074753+daytwist@users.noreply.github.com>
2024-09-10 11:35:18 +0900

    contentカラムをtextにリネーム

--- memo/app/models.py
+++ memo/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Memo(models.Model):
     title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
+    text = models.TextField(blank=True)
     created_datetime = models.DateTimeField(auto_now_add=True)
     updated_datetime = models.DateTimeField(auto_now=True)
 
